ecc25e1eb49916445fc01569218a4514fee25c99
matias <matiasureta28@gmail.com>
2023-11-07 20:13:58 -0300

    ultimos detalles de la pre entrega 1 de Js

--- main.js
+++ main.js
@@ -4,24 +4,43 @@ let comprarEuro = 989;
 let venderEuro = 936;
 
 while (true) {
-    const user = prompt("Ingresa tu nombre: ");
-    alert("Bienvenido" + " "+ user);
-    let consultaOperacion = prompt("¿Que operación desea realizar?: (Responda: comprar y/o vender");
-    let consultaDivisa = prompt("¿Quiere operar dolar o euro?: ");
+    const USUARIO = prompt("Ingresa tu nombre: ");
+    alert("Bienvenido" + " "+ USUARIO);
+
+    let consultaOperacion;
+    while (true) {
+        consultaOperacion = prompt("¿Que operación desea realizar?: (Responda: comprar y/o vender");
+        if (consultaOperacion === "comprar" || consultaOperacion === "vender") {
+            break;
+        } else {
+            alert("Por favor, responda exactamente 'comprar' o 'vender'");
+        }
+    }
+
+    let consultaDivisa;
+    while (true) {
+        consultaDivisa = prompt("¿Quiere operar dolar o euro?: ");
+        if (consultaDivisa === "dolar" || consultaDivisa === "euro") {
+            break;
+        } else {
+            alert("Por favor, responda exactamente 'dolar' o 'euro'");
+        }
+    }
+
     const CANTIDAD_PESOS= parseFloat(prompt("Ingresa el monto: "))
 
     if (consultaOperacion === "comprar" && consultaDivisa === "dolar") {
         let resultado = CANTIDAD_PESOS / comprarDolar;
-        alert("la cantidad a comprar es: "+" " + resultado);
+        alert("la cantidad de DOLARES a recibir es de: "+" " + resultado);
     } else if (consultaOperacion === "vender" && consultaDivisa === "dolar") {
         let resultado = CANTIDAD_PESOS * venderDolar;
-        alert("La cantidad a vender es: "+" " + resultado);
+        alert("La cantidad de PESOS a recibir es de: "+" " + resultado);
     } else if (consultaOperacion === "comprar" && consultaDivisa === "euro") {
         let resultado = CANTIDAD_PESOS / comprarEuro;
-        alert("la cantidad a comprar es: "+" " + resultado);
+        alert("la cantidad EUROS a recibir es de: "+" " + resultado);
     } else if (consultaOperacion === "vender" && consultaDivisa === "euro") {
         let resultado = CANTIDAD_PESOS * venderEuro;
-        alert("La cantidad a vender es: "+" " + resultado);
+        alert("La cantidad EUROS a recibir es de: "+" " + resultado);
     }
 
     let otraOperacion = prompt("¿Desea realizar otra operación? (Responda: si o no)");
@@ -30,5 +49,5 @@ while (true) {
     }
 }
 
-
 alert("Muchas gracias por usar nuestro servicio")
+
